244a3ccc9f7cb0f06aa898be40c2c57e630911b2
rjabdurrahman <md.rahman0296@gmail.com>
2019-06-19 00:16:20 +0600

    Added: Bootstarp layout

---
+++ precache-manifest.8dfffccc3534d98a97c65c013e7875a1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ce1e52b7a34163b12d37be2cb502d4b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cc3ec4ea6dcce7c6a5e9",
+    "url": "/static/css/main.8838ba68.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "cc3ec4ea6dcce7c6a5e9",
+    "url": "/static/js/main.3835f5ca.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
